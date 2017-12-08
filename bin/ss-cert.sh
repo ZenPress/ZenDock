193373dd00adaa470119d4b98aac18c1959205b9
Thiago Cangussu <thiago.cangussu@gmail.com>
2017-12-08 16:08:16 -0200

    Removed secrets folder

--- bin/ss-cert.sh
+++ bin/ss-cert.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-openssl genrsa -out "./secrets/ssl/local.key" 2048
+openssl genrsa -out "./config/ssl/local.key" 2048
 
-openssl req -new -key "./secrets/ssl/local.key" \
-  -out "./secrets/ssl/local.csr" \
+openssl req -new -key "./config/ssl/local.key" \
+  -out "./config/ssl/local.csr" \
   -subj "/C=BR/ST=Sao_Paulo/L=Sao_Paulo/O=OrgName/OU=IT Department/CN=example.com"
 
 openssl x509 -req -days 365 \
-  -in "./secrets/ssl/local.csr" \
-  -signkey "./secrets/ssl/local.key" \
-  -out "./secrets/ssl/local.crt"
+  -in "./config/ssl/local.csr" \
+  -signkey "./config/ssl/local.key" \
+  -out "./config/ssl/local.crt"
 
-rm -rf "./secrets/ssl/local.csr"
+rm -rf "./config/ssl/local.csr"
